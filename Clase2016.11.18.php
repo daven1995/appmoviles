be89e9017d660e092f7ca93b20dc47c910802157
David Davila <daviddavila11@hotmail.com>
2016-11-23 12:08:14 -0500

    Añadir algunas clases

---
+++ Clase2016.11.18.php
@@ -0,0 +1,12 @@
+Clase2016.11.18
+
+PHP gener archivos JSON
+Entonces si necesito algo, IONIC se conecta al servidor PHP y esa a su vez a My SQL
+y extrae los datos
+
+Una vez descargado el lamp, xamp, o wamp
+se ejecuta y SE LEVANTA EL SERVIDOR EN EL UERTO 8080
+y el My Sql se levanta en el 3037
+
+Se usa PHP solo para generar JSON
+
---
+++ index.php
@@ -0,0 +1,11 @@
+{
+"ci":"1718819699",
+"nombre":"luis",
+"calificacion":[
+	{"descripcion":"control1",
+	"valor":10},
+	{"descripcion":"examen",
+	"valor":8}
+
+	]
+}
