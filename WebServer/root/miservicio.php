1561c88cc47266e162a0baa27b904b7ab08722c7
David Davila <daviddavila11@hotmail.com>
2016-12-07 18:36:37 -0500

    Proyecto Final

--- WebServer/root/miservicio.php
+++ WebServer/root/miservicio.php
@@ -1,5 +1,13 @@
 <?php
 	$d=file_get_contents('php://input');
 	$dato=json_decode($d,true);
-	echo '{"respuesta":400}'
+	$db=mysql_connect('127.0.0.1','root','usbw');
+	mysql_select_db('estudiante',$db);
+	$rs=mysql_query('select nombre from estudiante',$db);
+	$nr=mysql_num_rows($rs);
+	if($nr>0){
+		$reg=mysql_fetch_row($rs);
+		echo '{"respuesta":"'.$reg[0].'"}';
+	}
+	mysql_close($db);
 ?>
